605d51b81286153c6326c211330915018bf1c358
Pasta-King <caiopasta@gmail.com>
2025-11-19 03:26:31 +0000

    Training Classifier

--- spikeClassification.py
+++ spikeClassification.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 import scipy.io as spio
 import tensorflow as tf
 import keras
+from keras import datasets, layers, models, backend, losses
 
-model_version = 2
+model_version = 4
 
 mat = spio.loadmat("Coursework-Datasets-20251028/D1.mat")
 d = mat["d"]
@@ -15,12 +16,7 @@ sequence_len = len(d[0])
 train_start = 0
 train_end = int(sequence_len * 0.8)
 
-
-d_zeroes = np.zeros(sequence_len, dtype=np.float64)
-for i in range(0, len(Index[0])):
-    d_zeroes[Index[0][i]] = Class[0][i]
-
-win_size = 200
+win_size = 100
 input_shape = (win_size, 1)
 win_step = 160
 
@@ -30,20 +26,66 @@ for i in range(train_start, sequence_len - win_size, win_step):
 
 d_input = np.array(d_input).reshape(-1, win_size)
 
-model = keras.models.load_model("models/spike_detection_v" + str(model_version) + ".keras")
+# detection_model = keras.models.load_model("models/spike_detection_v" + str(model_version) + ".keras")
 
-output = model.predict(d_input)
+# output = detection_model.predict(d_input)
 
-class_train = []
-class_label = []
-relu_flat_output = []
-error_spikes = 0
+train_data = []
+label_data = []
 
-# For SparseCategoricalCrossEntropy
-for i in range (0, len(output)):
-    for x in range(0, win_step):
-        if output[i][x][1] >  output[i][x][0]:
-            class_train.append(d[i - 100: i + 100])
-            class_label = d_zeroes[i * 160 + x]
+num_of_spikes = len(Index[0])
+
+for i in range(0, num_of_spikes):
+    spike_index = Index[0][i]
+    if (spike_index + win_size//2 < sequence_len) and (spike_index - win_size//2 >= 0):
+        train_data.append(d[0][spike_index - win_size//2 : spike_index + win_size//2 ])
+    elif ((spike_index + win_size//2 < sequence_len)):
+        train_data.append(d[0][: spike_index + win_size//2 ])
+    elif (spike_index - win_size//2 >= 0):
+        train_data.append(d[0][spike_index - win_size//2 :])
+    else:
+        train_data.append(d[0][:])
+
+    class_options = [0, 0, 0, 0, 0]
+    class_options[Class[0][i] - 1] = 1
+    label_data.append(class_options)
+
+val_class_train = np.array(train_data[int(num_of_spikes * 0.8):]).reshape(-1, win_size) 
+val_class_label = np.array(label_data[int(num_of_spikes * 0.8):])
+
+class_train = np.array(train_data[:int(num_of_spikes * 0.8)]).reshape(-1, win_size)
+class_label = np.array(label_data[:int(num_of_spikes * 0.8)])
+
+model = models.Sequential()
+model.add(layers.Input(shape=input_shape))
+model.add(layers.Conv1D(20, 3, padding="same", activation="relu"))
+model.add(layers.MaxPooling1D(4))
+model.add(layers.Conv1D(30, 3, padding="same", activation="relu"))
+model.add(layers.Conv1D(30, 3, padding="same", activation="relu"))
+#model.add(layers.MaxPooling1D(4))
+# model.add(layers.Conv1D(50, 10, padding="same", activation="relu"))
+# model.add(layers.MaxPooling1D(4))
+model.add(layers.Flatten())
+# model.add(layers.Dense(600, activation="relu"))
+model.add(layers.Dense(30, activation="relu"))
+model.add(layers.Dense(30, activation="relu"))
+model.add(layers.Dense(20, activation="relu"))
+model.add(layers.Dense(5, activation="sigmoid"))
+model.summary()
+
+model.compile(optimizer='adamW', loss=losses.CategoricalCrossentropy(), metrics=["accuracy"])
+
+# print(class_train)
+
+history = model.fit(class_train, class_label, epochs=60, batch_size=16, validation_data=(val_class_train, val_class_label)) 
+
+model.save("models/spike_classification_v" + str(model_version) + ".keras")
+
+# # For SparseCategoricalCrossEntropy
+# for i in range (0, len(output)):
+#     for x in range(0, win_step):
+#         if output[i][x][1] >  output[i][x][0]:
+#             class_train.append(d[i - 100: i + 100])
+#             class_label = d_zeroes[i * 160 + x]
 
 
